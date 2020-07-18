d30ead4bb1fa130753f02c049ada88c56ee52e9f
trevorism <trevorism@gmail.com>
2020-07-18 14:09:28 -0400

    Fix acceptance tests that were not running

--- src/acceptance/groovy/com/trevorism/eventhub/AdminDefinitions.groovy
+++ src/acceptance/groovy/com/trevorism/eventhub/AdminDefinitions.groovy
@@ -23,69 +23,69 @@ PasswordProvider passwordProvider = new PasswordProvider()
 String topicCreationResponse
 String subscriptionCreationResponse
 
-Given(/the topic {topic} does not exist/) { String topic ->
+Given(~/^the topic "([^"]*)" does not exist$/) { String topic ->
     topicUnderTest = topic
     ResponseUtils.closeSilently jsonHttpClient.delete("${baseUrl}/admin/topic/${topic}", ["Authorization":passwordProvider.password])
 }
 
-Given(/the topic {topic} already exists or is created/) { String topic ->
+Given(~/^the topic "([^"]*)" already exists or is created$/) { String topic ->
     topicUnderTest = topic
     ResponseUtils.closeSilently jsonHttpClient.post("${baseUrl}/admin/topic", topic, ["Authorization":passwordProvider.password])
 }
 
-Given(/the subscription {subscription}  already exists or is created/) { String subscription ->
+Given(~/^the subscription "([^"]*)"  already exists or is created$/) { String subscription ->
     Subscriber subscriber = new Subscriber(subscription, topicUnderTest, "http://event.trevorism.com/hook/store")
     ResponseUtils.closeSilently jsonHttpClient.post("${baseUrl}/admin/subscription", gson.toJson(subscriber), ["Authorization":passwordProvider.password])
 }
 
-When(/the topic {topic} is created$/) { String topic ->
+When(~/^the topic "([^"]*)" is created$/) { String topic ->
     topicCreationResponse = ResponseUtils.getEntity(jsonHttpClient.post("${baseUrl}/admin/topic", topic, ["Authorization":passwordProvider.password]))
 }
 
-When(/the subscription {subscription} is deleted/) { String subscription ->
+When(~/^the subscription "([^"]*)" is deleted$/) { String subscription ->
     ResponseUtils.closeSilently jsonHttpClient.delete("${baseUrl}/admin/subscription/${subscription}", ["Authorization":passwordProvider.password])
 }
 
-When(/the subscription {subscription} is created/) { String subscription ->
+When(~/^the subscription "([^"]*)" is created$/) { String subscription ->
     Subscriber subscriber = new Subscriber(subscription, topicUnderTest, "https://trevorism-eventhub.appspot.com/hook/store")
     ResponseUtils.closeSilently jsonHttpClient.post("${baseUrl}/admin/subscription", gson.toJson(subscriber), ["Authorization":passwordProvider.password])
 }
 
-When(/the topic {topic} is deleted/) { String topic ->
+When(~/^the topic "([^"]*)" is deleted$/) { String topic ->
     ResponseUtils.closeSilently jsonHttpClient.delete("${baseUrl}/admin/topic/${topic}", ["Authorization":passwordProvider.password])
 }
 
-When(/a subscription with a malformed url is created/) { ->
+When(~/^a subscription with a malformed url is created$/) { ->
     Subscriber subscriber = new Subscriber("errorSubscription", topicUnderTest, "googlyboogly")
     subscriptionCreationResponse = ResponseUtils.getEntity(jsonHttpClient.post("${baseUrl}/admin/subscription", gson.toJson(subscriber), ["Authorization":passwordProvider.password]))
 }
 
-Then(/the topic {topic} cannot be found/) { String topic ->
+Then(~/^the topic "([^"]*)" cannot be found$/) { String topic ->
     Thread.sleep(2000)
     assert !ResponseUtils.getEntity(jsonHttpClient.get("${baseUrl}/admin/topic", ["Authorization":passwordProvider.password])).contains(topic)
 }
 
-Then(/the topic {topic} exists/) { String topic ->
+Then(~/^the topic "([^"]*)" exists$/) { String topic ->
     Thread.sleep(2000)
     assert ResponseUtils.getEntity(jsonHttpClient.get("${baseUrl}/admin/topic/", ["Authorization":passwordProvider.password])).contains(topic)
 }
 
-Then(/the subscription {subscription} exists/) { String subscription ->
+Then(~/^the subscription "([^"]*)" exists$/) { String subscription ->
     Thread.sleep(2000)
     assert ResponseUtils.getEntity(jsonHttpClient.get("${baseUrl}/admin/subscription/${subscription}", ["Authorization":passwordProvider.password])).contains(subscription)
 }
 
-Then(/the subscription {subscription} does not exist/) { String subscription ->
+Then(~/^the subscription "([^"]*)" does not exist$/) { String subscription ->
     Thread.sleep(2000)
     assert !ResponseUtils.getEntity(jsonHttpClient.get("${baseUrl}/admin/subscription", ["Authorization":passwordProvider.password])).contains(subscription)
 }
 
-Then(/an error is returned, indicating the topic already exists/) { ->
+Then(~/^an error is returned, indicating the topic already exists$/) { ->
     Thread.sleep(2000)
     assert topicCreationResponse == "false"
 }
 
-Then(/an error is returned, indicating the subscription could not be created/) { ->
+Then(~/^an error is returned, indicating the subscription could not be created$/) { ->
     Thread.sleep(1000)
     assert subscriptionCreationResponse == "false"
 }
